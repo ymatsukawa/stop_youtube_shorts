0393726261e4097235d45006901ce38db22facb4
ymatsukawa <ymatsukawa27@gmail.com>
2023-05-05 19:55:05 +0900

    modified event listener

--- content.js
+++ content.js
@@ -2,7 +2,8 @@ function isShort() {
   return location.href.split("/").includes("shorts")
 }
 
-window.addEventListener('load', () => {
+// https://stackoverflow.com/a/34100952
+window.addEventListener('yt-navigate-start', () => {
   if(isShort()) {
     document.body.innerHTML = '<font size="32">Code it NOW</font>'
   }
